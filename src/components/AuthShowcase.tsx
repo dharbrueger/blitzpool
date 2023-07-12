218f514e62c7eec50fd61df21dd6a6ac947c05ce
daniel h <dharbrueger@gmail.com>
2023-07-12 14:18:08 -0400

    Style changes to AuthShowcase, still wip

--- src/components/AuthShowcase.tsx
+++ src/components/AuthShowcase.tsx
@@ -7,20 +7,18 @@ const AuthShowcase = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <>
+    <div className="text-white">
       {!sessionData && (
-        <div className="flex flex-col items-center justify-center gap-4">
           <div
-            className="flex cursor-pointer items-center rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+            className="cursor-pointer rounded-full outline bg-white/10 px-6 py-2 font-light transition hover:bg-white/20"
             onClick={() => void signIn()}
           >
-            Login
+            Log in
           </div>
-        </div>
       )}
       {sessionData && (
         <div className="flex items-center">
-          <div className="mr-4 flex cursor-pointer items-center rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+          <div className="text-black mr-4 flex cursor-pointer items-center rounded-full bg-white px-8 py-2 font-semibold no-underline transition hover:bg-white/80">
             {sessionData.user.image ? (
               <Image
                 src={sessionData.user.image}
@@ -30,21 +28,21 @@ const AuthShowcase = () => {
                 className="rounded-full"
               />
             ) : (
-              <FaUser className="h-8 w-8 rounded-full" />
+              <FaUser className="h-6 w-6 rounded-full" />
             )}
-            <div className="ml-4 font-thin text-white">
+            <div className="ml-4 font-thin">
               <Link href="/profile">{sessionData.user.name}</Link>
             </div>
           </div>
           <div
-            className="flex cursor-pointer items-center rounded-full bg-white/10 px-10 py-3 font-thin text-white no-underline transition hover:bg-white/20"
+            className="flex cursor-pointer items-center rounded-full bg-white/10 px-10 py-2 font-thin no-underline transition hover:bg-white/20"
             onClick={() => void signOut()}
           >
             Logout
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
