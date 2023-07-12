4ac870403b1497f020f753e5cb9b7e6812bd34e0
daniel h <dharbrueger@gmail.com>
2023-07-12 14:20:15 -0400

    Formatting, naming, and applied gradient globally (duh)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,22 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import dynamic from "next/dynamic";
-const Navbar = dynamic(() => import('~/components/Navbar'), {
-  ssr: false
+const Navbar = dynamic(() => import("~/components/Navbar"), {
+  ssr: false,
 });
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const Blitzpool: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Navbar />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div className="bg-gradient-to-b from-[#202232] to-[#0D0D10]">
+      <SessionProvider session={session}>
+        <Navbar />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
   );
 };
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(Blitzpool);
