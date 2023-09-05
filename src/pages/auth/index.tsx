c30bb851967beb96d8c7e5a2bd83777d7ac21776
Daniel <dharbrueger@gmail.com>
2023-09-05 09:14:46 -0400

    tweak mt

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -7,7 +7,7 @@ export default function Auth() {
   return (
     <>
       <Head>
-        <title>blitzpool - log in / sign up</title>
+        <title>blitzpool - sign in</title>
         <meta
           name="description"
           content="Join Blitzpool, the ultimate NFL pick 'em site! Gain access to game schedules updated in real time, make weekly predictions, and compete with friends to see who can spot solid matchups with precision. Join the fun and enjoy the thrill of NFL football as you battle it out in Blitzpool official Pools or just amongst friends. Sign up now and prove yourself as the ultimate football oracle!"
@@ -15,7 +15,7 @@ export default function Auth() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col items-center justify-center">
-        <div className="mb-8">
+        <div className="mx-12 mb-8">
           <Image src={LogoImage} alt="The Blitzpool logo." quality={100} />
         </div>
         <div className="text-2xl font-light text-white">
