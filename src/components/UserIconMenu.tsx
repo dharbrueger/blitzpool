92150aef8545dd2c46f1c94b39a37b5ca0231ef9
Daniel <dharbrueger@gmail.com>
2023-09-05 22:59:29 -0400

    upgrade user icon menu :)

--- src/components/UserIconMenu.tsx
+++ src/components/UserIconMenu.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { FaUser, FaSignOutAlt } from "react-icons/fa";
-import Image from "next/image";
-import { signOut, useSession } from "next-auth/react";
+import { FaUser, FaSignOutAlt, FaBell } from "react-icons/fa";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 const UserIconMenu = () => {
-  const { data: sessionData } = useSession();
+  const {data: sessionData} = useSession();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -17,40 +17,34 @@ const UserIconMenu = () => {
 
   return (
     <div className="text-white">
-      {sessionData && (
-        <div className="relative inline-block text-black mr-4">
-          <div
-            className="flex cursor-pointer items-centerfont-semibold no-underline rounded-full outline outline-2 outline-white transitionhover:bg-white/80"
-            onClick={toggleMenu}
-          >
-            {sessionData.user.image ? (
-              <Image
-                src={sessionData.user.image}
-                width={36}
-                height={36}
-                alt="User Avatar"
-                className="rounded-full"
-              />
-            ) : (
-              <FaUser className="h-6 w-6 rounded-full" />
-            )}
+      <div className="flex mr-4 text-black">
+        <div
+          className="items-center font-semibold transitionhover:bg-white/80 flex cursor-pointer rounded-[20px] text-white mr-4"
+          onClick={toggleMenu}
+        >
+          <div className="flex items-center justify-center py-4 px-12 bg-[#283441] rounded-[80px]">
+            <FaUser className="h-6 w-6 mr-4" />
+            <div className='font-light'>{sessionData?.user.name}</div>
           </div>
-          {isMenuOpen && (
-            <div className="absolute right-0 mt-2 bg-white shadow-lg rounded-md divide-y divide-gray-100">
-              <a
-                onClick={() => {
-                  void signOut();
-                  closeMenu();
-                }}
-                className="px-8 py-2 hover:bg-gray-100 flex items-center"
-              >
-                <FaSignOutAlt className="mr-2"/>
-                Logout
-              </a>
-            </div>
-          )}
         </div>
-      )}
+        <div className="items-center font-semibold transitionhover:bg-white/80 flex cursor-pointer rounded-[20px] text-white">
+          <FaBell className="h-6 w-6 mr-4" />
+        </div>
+        {isMenuOpen && (
+          <div className="absolute right-16 mt-16 divide-y divide-gray-100 bg-[#283441] text-bp-primary shadow-lg rounded-[80px] cursor-pointer">
+            <div
+              onClick={() => {
+                void signOut();
+                closeMenu();
+              }}
+              className="flex items-center px-8 py-2"
+            >
+              <FaSignOutAlt className="mr-2" />
+              Log Out
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
