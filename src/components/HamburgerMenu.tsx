c15cd12927cc1940c748197987f68bd8d6ca7b67
Daniel <dharbrueger@gmail.com>
2023-09-07 00:21:04 -0400

    add cool guy submenu click away behavior

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import type { NavLink } from "~/shared/lib/types";
 import NavLinkRenderer from "./NavLinkRenderer";
 import LogoImage from "public/logo.png";
 import Image from "next/image";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Link from "next/link";
+import { useClickAway } from "react-use";
 
 interface HamburgerMenuProps {
   links: NavLink[];
@@ -12,45 +13,56 @@ interface HamburgerMenuProps {
 
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ links }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  function handleClickOutside() {
+    setIsMenuOpen(false);
+  }
+
+  useClickAway(menuRef, handleClickOutside);
+
   return (
-    <div className="flex w-full p-8 items-center">
+    <div className="flex w-full items-center p-8">
       <div className="max-w-[50%]">
         <Link href="/">
           <Image src={LogoImage} alt="The Blitzpool logo." quality={100} />
         </Link>
       </div>
       <div className="ml-auto">
-        <button onClick={toggleMenu} className="text-white focus:outline-none">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-8 w-8"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+        <div ref={menuRef}>
+          <button
             onClick={toggleMenu}
+            className="text-white focus:outline-none"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-        {isMenuOpen && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
           <div
-            className={`absolute right-8 top-30 rounded-[10px] bg-[#283441] px-6 py-2 text-white font-light shadow-md`}
+            className={`top-30 absolute right-8 rounded-[10px] bg-[#283441] px-6 py-2 font-light text-white shadow-md ${
+              !isMenuOpen ? "hidden" : ""
+            }`}
           >
             <ul>
               <NavLinkRenderer links={links} showIcons />
             </ul>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
--- src/components/UserIconMenu.tsx
+++ src/components/UserIconMenu.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import { FaUser, FaSignOutAlt, FaBell } from "react-icons/fa";
+import { useRef, useState } from "react";
+import { FaUser, FaSignOutAlt } from "react-icons/fa";
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
-
+import { useClickAway } from "react-use";
 const UserIconMenu = () => {
   const { data: sessionData } = useSession();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -15,30 +16,40 @@ const UserIconMenu = () => {
     setIsMenuOpen(false);
   };
 
+  function handleClickOutside() {
+    closeMenu();
+  }
+
+  useClickAway(menuRef, handleClickOutside);
+
   return (
     <div className="text-white">
       <div className="mr-4 flex text-black">
-        <div
-          className="transitionhover:bg-white/80 mr-4 flex cursor-pointer items-center rounded-[20px] font-semibold text-white"
-          onClick={toggleMenu}
-        >
-          <div className="flex items-center justify-center rounded-[80px] bg-[#283441] hover:bg-[#303f4e] px-12 py-4 select-none">
-            <FaUser className="mr-4 h-6 w-6" />
-            <div className="font-light">{sessionData?.user.name}</div>
+        <div ref={menuRef}>
+          <div
+            className="transitionhover:bg-white/80 mr-4 flex cursor-pointer items-center rounded-[20px] font-semibold text-white"
+            onClick={toggleMenu}
+          >
+            <div className="flex select-none items-center justify-center rounded-[80px] bg-[#283441] px-12 py-4 hover:bg-[#303f4e]">
+              <FaUser className="mr-4 h-6 w-6" />
+              <div className="font-light">{sessionData?.user.name}</div>
+            </div>
           </div>
-        </div>
-        <div className="transitionhover:bg-white/80 flex cursor-pointer items-center rounded-[20px] font-semibold text-white">
+          {/* <div className="transitionhover:bg-white/80 flex cursor-pointer items-center rounded-[20px] font-semibold text-white">
           <FaBell className="mr-4 h-6 w-6" />
-        </div>
-        {isMenuOpen && (
-          <div className="absolute right-16 mt-16 cursor-pointer rounded-[10px] bg-[#283441] text-white shadow-lg">
+        </div> */}
+          <div
+            className={`absolute right-16 mt-2 cursor-pointer rounded-[10px] bg-[#283441] text-white shadow-lg ${
+              !isMenuOpen ? "hidden" : ""
+            }`}
+          >
             <button
               disabled
               onClick={() => {
                 void signOut();
                 closeMenu();
               }}
-              className="flex items-center px-8 py-2 uppercase text-slate-600 cursor-not-allowed"
+              className="flex cursor-not-allowed items-center px-8 py-2 uppercase text-slate-600"
             >
               <FaUser className="mr-2" />
               Account
@@ -54,7 +65,7 @@ const UserIconMenu = () => {
               Log Out
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
