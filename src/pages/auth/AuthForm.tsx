2f17f3880f5d504e87bf772ff33a41b56f1c7aff
daniel h <dharbrueger@gmail.com>
2023-09-24 17:39:59 -0400

    replace G logo

--- src/pages/auth/AuthForm.tsx
+++ src/pages/auth/AuthForm.tsx
@@ -4,71 +4,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { signIn } from "next-auth/react";
 
 const AuthForm = () => (
-  <Form.Root className="grid w-[320px]">
-    <Form.Field className="mb-[10px] grid" name="email">
-      <div className="flex items-baseline justify-between">
-        <Form.Label className="text-[15px] font-light leading-[35px] text-white">
-          email
-        </Form.Label>
-        <Form.Message
-          className="text-[13px] text-white opacity-[0.8]"
-          match="valueMissing"
-        >
-          Please enter your email
-        </Form.Message>
-        <Form.Message
-          className="text-[13px] text-white opacity-[0.8]"
-          match="typeMismatch"
-        >
-          Please provide a valid email
-        </Form.Message>
-      </div>
-      <Form.Control asChild>
-        <input
-          className="shadow-blackA9 selection:color-white selection:bg-blackA9 box-border inline-flex h-[35px] w-full appearance-none items-center justify-center rounded-[4px] bg-slate-400 px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
-          type="email"
-          required
-        />
-      </Form.Control>
-    </Form.Field>
-    <Form.Field className="mb-[10px] grid" name="password">
-      <div className="flex items-baseline justify-between">
-        <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-          password
-        </Form.Label>
-        <Form.Message
-          className="text-[13px] text-white opacity-[0.8]"
-          match="valueMissing"
-        >
-          Please enter your password
-        </Form.Message>
-      </div>
-      <Form.Control asChild>
-        <input
-          className="shadow-blackA9 selection:color-white selection:bg-blackA9 mb-[15px] box-border inline-flex h-[35px] w-full resize-none appearance-none items-center justify-center rounded-[4px] bg-slate-400 px-[10px] text-[15px] leading-none text-black shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
-          type="password"
-          required
-        />
-      </Form.Control>
-    </Form.Field>
-    <Form.Submit asChild className="mb-5">
-      <button className="text-violet11 shadow-blackA7 hover:bg-mauve3 box-border inline-flex h-[35px] w-2/5 items-center justify-center place-self-center rounded-[4px] bg-bp-primary px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
-        Sign In
-      </button>
-    </Form.Submit>
     <div
       onClick={() => void signIn('google', { callbackUrl: '/' })}
-      className="text-violet11 shadow-blackA7 hover:bg-mauve3 mb-[75px] box-border inline-flex h-[35px] w-3/5 cursor-pointer items-center justify-center place-self-center rounded-[4px] bg-white font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none"
+      className="p-4 cursor-pointer items-center justify-center place-self-center rounded-[4px] bg-white font-medium focus:outline-bp-primary"
     >
-      <i className="fab fa-google pr-3"></i> Sign In with Google
+      <div className="flex flex-row gap-2"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/><path d="M1 1h22v22H1z" fill="none"/></svg>
+      Sign In with Google
+      </div>
     </div>
-    <div className="text-light place-self-center text-gray-600">
-      <span className="pr-[5px]">don&apos;t have an account?</span>
-      <a href="/signup" className="text-white">
-        sign up
-      </a>
-    </div>
-  </Form.Root>
 );
 
 export default AuthForm;
